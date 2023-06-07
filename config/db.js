d322300caa55f745172cae0077d0b9fd28732e25
Jes Jes <jvillalont94@gmail.com>
2023-06-06 20:39:46 -0600

    con env

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 import Sequelize from "sequelize";
 import dotenv from 'dotenv/config';
 
-const db = new Sequelize('railway', 'root', 'T5tTxwACDQQBxOQBQRs1', {
-    host: 'containers-us-west-84.railway.app',
-    port: '6221',
+const db = new Sequelize(process.env.DB_NAME, process.env.USER, process.env.PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     define: {
         timestamps:false
